701513b839f73c4dd22cb3a7c532c24f62b56c46
Cécile Bourguignon <sapho89@gmail.com>
2021-06-22 08:17:45 +1200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'myRocketLauncher-cache-v1';
+var urlsToCache = [];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return CACHE_NAME !== cacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
